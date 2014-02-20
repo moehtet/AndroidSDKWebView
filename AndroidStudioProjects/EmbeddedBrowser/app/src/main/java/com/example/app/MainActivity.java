79081b637ad1e03bba5076cb5844eb4189909771
moehtet <moehtet@outlook.com>
2014-02-20 11:21:07 +0800

    changed text button to image button  ,  layout

--- AndroidStudioProjects/EmbeddedBrowser/app/src/main/java/com/example/app/MainActivity.java
+++ AndroidStudioProjects/EmbeddedBrowser/app/src/main/java/com/example/app/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.app;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -17,29 +21,77 @@ import android.os.Build;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
     WebView embeddedBrowser;
-    Button backBtn;
-    Button forwardBtn;
-    Button reloadBtn;
-    Button closeBtn;
+    ImageButton backBtn;
+    ImageButton forwardBtn;
+    ImageButton reloadBtn;
+    ImageButton closeBtn;
+    ImageButton shareBtn;
+    String shareUrl;
+    String shareTitle;
+
+    final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        shareUrl="http://nodejs.org/";
         embeddedBrowser = (WebView) findViewById(R.id.webView);
         embeddedBrowser.getSettings().setJavaScriptEnabled(true);
         embeddedBrowser.getSettings().setBuiltInZoomControls(true);
         embeddedBrowser.getSettings().setDisplayZoomControls(false);
         embeddedBrowser.setWebViewClient(new myWebClient());
-        embeddedBrowser.loadUrl("http://nodejs.org/");
+        embeddedBrowser.loadUrl(shareUrl);
 
-        backBtn = (Button) findViewById(R.id.back);
-        forwardBtn=(Button) findViewById(R.id.forward);
-        reloadBtn=(Button) findViewById(R.id.reload);
-        closeBtn=(Button)findViewById(R.id.closeBtn);
+        backBtn = (ImageButton) findViewById(R.id.back);
+        forwardBtn=(ImageButton) findViewById(R.id.forward);
+        reloadBtn=(ImageButton) findViewById(R.id.reloadBtn);
+        closeBtn=(ImageButton)findViewById(R.id.closeBtn);
+        shareBtn=(ImageButton)findViewById(R.id.shareBtn);
+
+        //////////////////////
+
+
+        ImageButton alertBtn=(ImageButton)findViewById(R.id.alertBtn);
+        alertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final CharSequence[] items = {"Open in Browser", "Share to Other Apps"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        if(item==0){
+                            Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(shareUrl));
+                            startActivity(intent);
+                        }
+                        else{
+                            Intent sendIntent = new Intent();
+                            sendIntent.setAction(Intent.ACTION_SEND);
+                            sendIntent.putExtra(Intent.EXTRA_SUBJECT,shareTitle);
+                            sendIntent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+                            //sendIntent.putExtra(Intent.EXTRA_STREAM,shareUrl);
+                            sendIntent.setType("text/plain");
+                            // startActivity(sendIntent);
+                            startActivity(Intent.createChooser(sendIntent, "How do you want to share?"));
+
+                        }
+                    }
+                });
+                AlertDialog alert = builder.create();
+
+                alert.show();
+            }
+        });
+        //////////////////////
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -69,6 +121,22 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), Home.class);
                 startActivityForResult(myIntent, 0);
+            }
+        });
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT,shareTitle);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+                //sendIntent.putExtra(Intent.EXTRA_STREAM,shareUrl);
+                sendIntent.setType("text/plain");
+               // startActivity(sendIntent);
+                startActivity(Intent.createChooser(sendIntent, "How do you want to share?"));
+
+
             }
         });
     }
@@ -102,12 +170,15 @@ public class MainActivity extends ActionBarActivity {
             catch (Exception exception){
                 exception.printStackTrace();
             }
+
+            shareTitle=view.getTitle();
+
         }
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             // TODO Auto-generated method stub
-
+            shareUrl=url;
             view.loadUrl(url);
            // this.onLoadResource(view,url);
             return true;
