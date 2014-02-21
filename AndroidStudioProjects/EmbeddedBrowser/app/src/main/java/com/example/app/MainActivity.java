a3873e50eb40b821f13d09d921bc23e600bb54c9
moehtet <moehtet@outlook.com>
2014-02-21 10:13:09 +0800

    just delete comment part

--- AndroidStudioProjects/EmbeddedBrowser/app/src/main/java/com/example/app/MainActivity.java
+++ AndroidStudioProjects/EmbeddedBrowser/app/src/main/java/com/example/app/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.app;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.LabeledIntent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
@@ -23,6 +28,9 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
     WebView embeddedBrowser;
@@ -127,15 +135,62 @@ public class MainActivity extends ActionBarActivity {
         shareBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_SUBJECT,shareTitle);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, shareUrl);
-                //sendIntent.putExtra(Intent.EXTRA_STREAM,shareUrl);
-                sendIntent.setType("text/plain");
-               // startActivity(sendIntent);
-                startActivity(Intent.createChooser(sendIntent, "How do you want to share?"));
 
+                Resources resources = getResources();
+
+                Intent emailIntent = new Intent();
+                emailIntent.setAction(Intent.ACTION_SEND);
+                // Native email client doesn't currently support HTML, but it doesn't hurt to try in case they fix it
+                emailIntent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, shareTitle);
+                emailIntent.setType("message/rfc822");
+
+                PackageManager pm = getPackageManager();
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+
+
+                Intent openInChooser = Intent.createChooser(emailIntent, "Chooser");
+
+                List<ResolveInfo> resInfo = pm.queryIntentActivities(sendIntent, 0);
+                List<LabeledIntent> intentList = new ArrayList<LabeledIntent>();
+                for (int i = 0; i < resInfo.size(); i++) {
+                    // Extract the label, append it, and repackage it in a LabeledIntent
+                    ResolveInfo ri = resInfo.get(i);
+                    String packageName = ri.activityInfo.packageName;
+                    if(packageName.contains("android.email")) {
+                        emailIntent.setPackage(packageName);
+                    } else if(packageName.contains("plus") || packageName.contains("twitter") || packageName.contains("facebook") || packageName.contains("mms") || packageName.contains("android.gm")) {
+                        Intent intent = new Intent();
+                        intent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
+                        intent.setAction(Intent.ACTION_SEND);
+                        intent.setType("text/plain");
+                        if(packageName.contains("twitter")) {
+                            intent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+                        } else if(packageName.contains("facebook")) {
+                            // Warning: Facebook IGNORES our text. They say "These fields are intended for users to express themselves. Pre-filling these fields erodes the authenticity of the user voice."
+                            // One workaround is to use the Facebook SDK to post, but that doesn't allow the user to choose how they want to share. We can also make a custom landing page, and the link
+                            // will show the <meta content ="..."> text from that page with our link in Facebook.
+                            intent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+                        } else if(packageName.contains("mms")) {
+                            intent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+                        } else if(packageName.contains("android.gm")) {
+                            intent.putExtra(Intent.EXTRA_TEXT, shareUrl);
+                            intent.putExtra(Intent.EXTRA_SUBJECT, shareTitle);
+                            intent.setType("message/rfc822");
+                        } else if(packageName.contains("plus")){
+                            intent.putExtra(Intent.EXTRA_TEXT,shareUrl);
+                            intent.putExtra(Intent.EXTRA_SUBJECT, shareTitle);                       }
+
+                        intentList.add(new LabeledIntent(intent, packageName, ri.loadLabel(pm), ri.icon));
+                    }
+                }
+
+                // convert intentList to array
+                LabeledIntent[] extraIntents = intentList.toArray( new LabeledIntent[ intentList.size() ]);
+
+                openInChooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents);
+                startActivity(openInChooser);
 
             }
         });
